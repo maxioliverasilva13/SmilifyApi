83e529e3f248a03d774dd14ad05770bc07ae4014
Maximiliano Olivera <maximiliano.olivera@dualbootpartners.com>
2023-04-26 00:51:54 -0300

    Finish implementation of /current_user endpoint to get user info by token , and configure cors

--- src/main/java/util/JWT.java
+++ src/main/java/util/JWT.java
@@ -10,6 +10,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 import javax.json.Json;
 import javax.json.JsonObject;
 
@@ -18,37 +19,51 @@ import javax.json.JsonObject;
  * @author rodrigo
  */
 public class JWT {
-    private String KEY = "mi_clave";
-    private static  JWT instance;
 
-    public JsonObject encode(Map<String, Object> claims){
+    public static String KEY = "SECRETCLAVE";
+    private static JWT instance;
+    static byte[] secret = KEY.getBytes();
+
+    public JsonObject encode(Map<String, Object> claims) {
         long current_time = System.currentTimeMillis();
-        String jwt = Jwts.builder().signWith(SignatureAlgorithm.HS256,KEY).setIssuedAt(new Date(current_time)).setExpiration(new Date(current_time + 86400000)).addClaims(claims).compact();
-        JsonObject json = Json.createObjectBuilder().add("result",jwt).build();
-          
+        String jwt = Jwts.builder().signWith(SignatureAlgorithm.HS256, KEY.getBytes()).setIssuedAt(new Date(current_time)).setExpiration(new Date(current_time + 86400000)).addClaims(claims).compact();
+        JsonObject json = Json.createObjectBuilder().add("result", jwt).build();
 
         return json;
     }
-    
-    public Long getUserIdByToken(String token){
 
-        Claims claims = Jwts.parser()
-                        .setSigningKey(KEY.getBytes()) // clave secreta para verificar la firma del JWT
-                        .parseClaimsJws(token)
-                        .getBody();
+    public Long getUserIdByToken(String token) {
+        Claims claims = extractClaims(token);
         Long id = claims.get("id", Long.class);
         return id;
 
     }
-   
+
+    public boolean isExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+        final Claims claim = extractClaims(token);
+        return claimResolver.apply(claim);
+    }
+
+    private static Claims extractClaims(String token) {
+        return Jwts.parser().setSigningKey(KEY.getBytes()).parseClaimsJws(token).getBody();
+    }
+
     public static JWT getInstance() {
         if (instance == null) {
             instance = new JWT();
         }
         return instance;
     }
-    
+
     private JWT() {
-        
+
     }
 }
